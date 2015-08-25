a9d7f28977857d801407f982ccc588aecae9296e
Martin Joiner <martin@carmendata.co.uk>
2015-08-25 12:45:27 +0100

    Converted root index to PHP

---
+++ httpdocs/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>Testing Server</title>
+
+	<link rel="stylesheet" href="/css/style.css">
+
+</head>
+<body>
+
+	<h1>Testing VHost</h1>
+
+	<ul>
+		<?php
+		$arrFolders = scandir($_SERVER['DOCUMENT_ROOT']);
+		$arrIgnoredFolders = array('Packages', 'node_modules', 'js', 'css', 'page', '.', '..' );
+		foreach( $arrFolders as $thisFolder ){
+			if( !in_array($thisFolder, $arrIgnoredFolders ) ){
+				$spaceyName = preg_replace('/-/', ' ', $thisFolder);
+				print '<li><a href="' . $thisFolder . '">' . $spaceyName . '</a></li>';
+			}
+		}
+		?>
+		
+	</ul>
+
+	<p>Martin would like a cup of tea please</p>
+
+</body>
+</html>
